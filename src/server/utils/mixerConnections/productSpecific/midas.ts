f464e7c528d49f753a6c332b9b98f50422cb6738
Jesper Stærkær <jesper@starkar.no>
2020-04-23 11:47:10 +0200

    chore: lint:fix

--- src/server/utils/mixerConnections/productSpecific/midas.ts
+++ src/server/utils/mixerConnections/productSpecific/midas.ts
@@ -1,48 +1,40 @@
 import { store, state } from '../../../reducers/store'
 import { socketServer } from '../../../expressHandler'
 
-import { SET_VU_REDUCTION_LEVEL, SET_VU_LEVEL } from  '../../../reducers/faderActions'
-import { SOCKET_SET_VU, SOCKET_SET_VU_REDUCTION } from '../../../constants/SOCKET_IO_DISPATCHERS';
+import { SET_VU_REDUCTION_LEVEL, SET_VU_LEVEL } from '../../../reducers/faderActions'
+import { SOCKET_SET_VU, SOCKET_SET_VU_REDUCTION } from '../../../constants/SOCKET_IO_DISPATCHERS'
 
 export const midasMeter = (message: any) => {
+	const DATA_OFFSET = 4
+	const uint8bytes = Uint8Array.from(message[0])
+	const dataview = new DataView(uint8bytes.buffer)
+	const vuMeters = []
+	let level: number
+	let reductionLevel: number
+	const numberOfChannels = state.settings[0].numberOfChannelsInType[0]
 
-    const DATA_OFFSET = 4;
-    let uint8bytes = Uint8Array.from(message[0]);
-    let dataview = new DataView(uint8bytes.buffer);
-    let vuMeters = []
-    let level: number
-    let reductionLevel: number
-    let numberOfChannels = state.settings[0].numberOfChannelsInType[0];
+	for (let i = 0; i < numberOfChannels; i++) {
+		level = dataview.getFloat32(4 * i + DATA_OFFSET, true)
+		reductionLevel = dataview.getFloat32(4 * (i + 64) + DATA_OFFSET, true)
 
-    for (let i=0; i < numberOfChannels; i++) {
-        level = dataview.getFloat32(4*i+DATA_OFFSET , true)
-        reductionLevel = dataview.getFloat32(4*(i+64)+DATA_OFFSET , true)
-
-        vuMeters.push({vuVal : level})
-        store.dispatch({
-            type:SET_VU_LEVEL,
-            channel: i,
-            level: level
-        })
-        socketServer.emit(
-            SOCKET_SET_VU,
-            {
-                faderIndex: state.channels[0].channel[i].assignedFader,
-                level: level
-            }
-        )
-        store.dispatch({
-            type:SET_VU_REDUCTION_LEVEL,
-            channel: i,
-            level: reductionLevel
-        })
-        socketServer.emit(
-            SOCKET_SET_VU_REDUCTION,
-            {
-                faderIndex: state.channels[0].channel[i].assignedFader,
-                level: reductionLevel
-            }
-        )
-    }
+		vuMeters.push({ vuVal: level })
+		store.dispatch({
+			type: SET_VU_LEVEL,
+			channel: i,
+			level: level
+		})
+		socketServer.emit(SOCKET_SET_VU, {
+			faderIndex: state.channels[0].channel[i].assignedFader,
+			level: level
+		})
+		store.dispatch({
+			type: SET_VU_REDUCTION_LEVEL,
+			channel: i,
+			level: reductionLevel
+		})
+		socketServer.emit(SOCKET_SET_VU_REDUCTION, {
+			faderIndex: state.channels[0].channel[i].assignedFader,
+			level: reductionLevel
+		})
+	}
 }
-
